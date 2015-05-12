138ec6c4f6b11415dd68ff5d66a2dac08bee43f5
Michelle Steele <octoberclub@gmail.com>
2015-05-12 20:16:50 +0100

    Added loop

--- numbers.rb
+++ numbers.rb
@@ -1,10 +1,26 @@
 #!/usr/bin/env ruby
-x = Random.rand(1..10)
-y = Random.rand(1..10)
-print "#{x} + #{y} = "
-answer = gets.to_i
-if answer == x + y
-	puts "Right!"
-else
-	puts "Wrong!"
+turns = 0
+correct = 0
+wrong = 0
+duration = Time.now
+
+while turns < 5
+  	turns = turns + 1
+
+	x = Random.rand(1..10)
+	y = Random.rand(1..10)
+	print "#{x} + #{y} = "
+	answer = gets.to_i
+	if answer == x + y
+		correct = correct + 1
+	else
+		wrong = wrong + 1
+	end
+
+ 	# game code
 end
+score = 100 * correct / turns
+duration = Time.now - duration
+puts "Rights #{correct}; Wrongs #{wrong}; Score #{score}%"
+puts "Total time #{duration} seconds"
+puts "#{duration/turns} seconds per problem"
